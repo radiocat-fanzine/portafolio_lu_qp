61c09b6e57d934cae77f23fd47ae44c13c9b62e0
Luzmila Quezada <radiocat.fanzine@gmail.com>
2026-01-11 07:01:14 -0500

    app 2

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import Layout from './components/Layout'
 import ProjectLayout from './layout/ProjectLayout'
 
 import Home from './pages/Home'
-import Certificates from "./components/certificates/Certificates";
-
+import Certificates from './components/certificates/Certificates'
 import Product from './pages/Product'
 import UXResearch from './pages/UXResearch'
 import GraphicDesign from './pages/GraphicDesign'
@@ -12,26 +11,23 @@ import GraphicDesign from './pages/GraphicDesign'
 function App() {
   return (
     <Routes>
-      {/* Layout general */}
       <Route path="/" element={<Layout />}>
+        {/* Home */}
         <Route index element={<Home />} />
+
+        {/* Certificates */}
         <Route path="certificates" element={<Certificates />} />
-      </Route>
 
-      {/* ProjectLayout para proyectos individuales */}
-      <Route path="portfolio/product/*" element={<ProjectLayout />}>
-        <Route index element={<Product />} />
-      </Route>
-
-      <Route path="portfolio/ux-research/*" element={<ProjectLayout />}>
-        <Route index element={<UXResearch />} />
-      </Route>
-
-      <Route path="portfolio/graphic-design/*" element={<ProjectLayout />}>
-        <Route index element={<GraphicDesign />} />
+        {/* Portfolio */}
+        <Route path="portfolio" element={<ProjectLayout />}>
+          <Route path="product" element={<Product />} />
+          <Route path="ux-research" element={<UXResearch />} />
+          <Route path="graphic-design" element={<GraphicDesign />} />
+        </Route>
       </Route>
     </Routes>
   )
 }
 
 export default App
+
